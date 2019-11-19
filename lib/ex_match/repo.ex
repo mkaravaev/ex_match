938d3080f514dca1d831a1e24afec9b91bf14994
Misha <mkaravaev@gmail.com>
2019-11-19 18:31:45 +0500

    add: helper func for repo

--- lib/ex_match/repo.ex
+++ lib/ex_match/repo.ex
@@ -2,4 +2,14 @@ defmodule ExMatch.Repo do
   use Ecto.Repo,
     otp_app: :ex_match,
     adapter: Ecto.Adapters.Postgres
+
+  import Ecto.Query
+
+  def count(module) do
+    __MODULE__.aggregate(module, :count, :id)
+  end
+
+  def last(module) do
+    __MODULE__.one(from x in module, order_by: [desc: x.inserted_at], limit: 1)
+  end
 end
