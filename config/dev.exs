205854a8879a98174731d94ad3b32a8f258d1a09
Misha <mkaravaev@gmail.com>
2019-11-19 17:22:16 +0500

    remove: unnecessary slash in providers URI

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ use Mix.Config
 config :ex_match, :providers,
   fastball: [
     base_url:  "http://forzaassignment.forzafootball.com:8080",
-    path: "/feed/fastball"
+    path: "feed/fastball"
   ],
   matchbeam: [
     base_url: "http://forzaassignment.forzafootball.com:8080",
-    path: "/feed/matchbeam"
+    path: "feed/matchbeam"
   ]
--- config/test.exs
+++ config/test.exs
@@ -3,10 +3,10 @@ use Mix.Config
 config :ex_match, :providers,
   fastball: [
     base_url:  "http://localhost:4080",
-    path: "/feed/fastball"
+    path: "feed/fastball"
   ],
   matchbeam: [
     base_url:  "http://localhost:4080",
-    path: "/feed/matchbeam"
+    path: "feed/matchbeam"
   ]
 
