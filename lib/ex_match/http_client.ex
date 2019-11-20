d50df3dba968877d8af8d194c852b397b16c33dc
Misha <mkaravaev@gmail.com>
2019-11-20 14:13:16 +0500

    fix: disable sending empty value query params

--- lib/ex_match/http_client.ex
+++ lib/ex_match/http_client.ex
@@ -2,16 +2,26 @@ defmodule ExMatch.HTTPClient do
   @moduledoc false
 
   def call(url, params) do
-    case HTTPoison.get(url, [], params: params) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Jason.decode!(body)}
+    params
+    |> remove_nil_params()
+    |> do_call(url)
+    |> case do
+         {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+           {:ok, Jason.decode!(body)}
 
-      {:ok, %HTTPoison.Response{status_code: 400}} ->
-        {:error, :invalid_params}
+         {:ok, %HTTPoison.Response{status_code: 400}} ->
+           {:error, :invalid_params}
 
-      {:ok, %HTTPoison.Response{status_code: 503}} ->
-        {:error, :temporarily_unavailable}
-    end
+         {:ok, %HTTPoison.Response{status_code: 503}} ->
+           {:error, :temporarily_unavailable}
+       end
   end
+
+  def remove_nil_params(params) do
+    Enum.reject(params, fn{_key, val} -> is_nil(val) end)
+  end
+
+  defp do_call([], url), do: HTTPoison.get(url)
+  defp do_call(params, url), do: HTTPoison.get(url, [], params: params)
 
 end
